d650cce8654fd8e8d8ab1183bb8628b8691e277f
Jeroen Jacobs <jacobsjeroen@gmail.com>
2011-02-27 00:13:34 +0100

    Added episode (number), title, summary and air_date to Episode.

--- lib/bierdopje/episode.rb
+++ lib/bierdopje/episode.rb
@@ -1,7 +1,7 @@
 module Bierdopje
   class Episode < Base
     def self.attributes_names
-      [:id, :season, :code, :show_name]
+      [:id, :season, :episode, :code, :title, :summary, :air_date, :show_name]
     end
     attr_accessor *attributes_names
 
@@ -13,7 +13,11 @@ module Bierdopje
       attributes = {
         :id => doc.at_xpath('episodeid').content,
         :season => doc.at_xpath('season').content,
+        :episode => doc.at_xpath('episode').content,
         :code => doc.at_xpath('formatted').content,
+        :title => doc.at_xpath('title').content,
+        :summary => doc.at_xpath('summary').content,
+        :air_date => doc.at_xpath('airdate').content,
         :show_name => doc.at_xpath('showlink').content.match(/^http:\/\/www\.bierdopje\.com\/shows\/(.*)$/)[1]
       }
       super attributes
--- spec/episodes_spec.rb
+++ spec/episodes_spec.rb
@@ -6,7 +6,11 @@ describe Bierdopje::Episode do
       episode = Bierdopje::Episode.find 163480
       episode.id.should == '163480'
       episode.season.should == '1'
+      episode.episode.should == '1'
       episode.code.should == 'S01E01'
+      episode.title.should == 'Pilot (1)'
+      episode.summary.should include('Stripped of everything, the 48 survivors scavenge what they can from the plane for their survival.')
+      episode.air_date.should == '22-09-2004'
       episode.show_name.should == 'lost'
       episode.show.name.should == 'Lost'
       episode.show.id.should == '5517'
